9e8d10d3e0b20ff8d2c421c6ad03a61762b6dc72
tessoka <akos.gollitz@gmail.com>
2021-11-24 19:47:55 +0100

    infinite-looooooop

--- react-and-morty/src/components/LocationCard.jsx
+++ react-and-morty/src/components/LocationCard.jsx
@@ -9,6 +9,7 @@ const LocationCard = ({loc}) => {
     <Link to={`/locations/${loc.id}`}>
       <div className="loc-card">
           <div className="loc-info">
+              <p>{loc.id}</p>
               <p><strong>Name:</strong><br/> {loc.name}</p>
               <p><strong>Type:</strong><br/> {loc.type}</p>
           </div>
--- react-and-morty/src/components/Locations.jsx
+++ react-and-morty/src/components/Locations.jsx
@@ -1,25 +1,51 @@
 import React from "react";
-// import { useState } from "react";
 import LocationCard from './LocationCard'
 import { useLocations } from '../api/useData';
-
+import { useState, useEffect } from "react";
+import InfiniteScroll from 'react-infinite-scroll-component';
+import axios from 'axios'
 
 
 const Locations = () => {
 
-  const [isPending, locations] = useLocations(1);
+  const [ pageNum, setPageNum ] = useState(2)
+  const [ isPending, locations ] = useLocations(pageNum);
+  const [ locationKeeper, setLocationKeeper ] = useState([])
+
+  const getLoc = async (pageNum) => {
+    const pageData = await axios.get("https://rickandmortyapi.com/api/location/?page=" + pageNum)
+    for (const locs of pageData.data.results) {
+      locationKeeper.push(locs)
+    }
+    
+    setLocationKeeper([...locationKeeper])
+  }
+
+  useEffect(() => {
+    getLoc()
+  },[])
+
+  console.log(locationKeeper)
+
+  const triggerLoad = () => {
+    setPageNum(pageNum + 1)
+    getLoc(pageNum)
+  }
+
+
 
   return ( 
-    <div className="loc-container">
+    <InfiniteScroll dataLength={locationKeeper.length} next={triggerLoad} hasMore={true} loader={"..."}>
+      <div className="loc-container">
 
-      { isPending && <div className="loading-center loading-bubble">Loading...</div>}
+        { isPending && <div className="loading-center loading-bubble">Loading...</div>}
 
-      { !isPending && locations.results.map(loc =>
-      <LocationCard loc={loc} key={loc.id}/>
-      )}
-      
-    </div>
-
+          { !isPending && locationKeeper.map(loc =>
+          <LocationCard loc={loc} key={loc.id}/>
+          )}
+          
+      </div>
+    </InfiniteScroll>
   )
 }
  
