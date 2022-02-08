c24e799240a035f086fbb019492617eead24c534
tessoka <akos.gollitz@gmail.com>
2022-02-08 19:13:56 +0100

    Updates

---
+++ precache-manifest.c9bb3beed29735eea7f1cdc82d1600ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d1f759226b9c046aef196e3c8fa4268",
+    "url": "/react-and-morty/index.html"
+  },
+  {
+    "revision": "16710a2ef344e9e7a4d4",
+    "url": "/react-and-morty/static/css/main.f17f10aa.chunk.css"
+  },
+  {
+    "revision": "120b464c6b6f5ec9d694",
+    "url": "/react-and-morty/static/js/2.0b7ca634.chunk.js"
+  },
+  {
+    "revision": "0e6cb1fbd5179720173d2f6b0067c84b",
+    "url": "/react-and-morty/static/js/2.0b7ca634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16710a2ef344e9e7a4d4",
+    "url": "/react-and-morty/static/js/main.32a10815.chunk.js"
+  },
+  {
+    "revision": "453b88591806e02178dd",
+    "url": "/react-and-morty/static/js/runtime-main.726c9020.js"
+  },
+  {
+    "revision": "31931b8159412e809ed0c124699d5b5f",
+    "url": "/react-and-morty/static/media/RMlogo_v3.31931b81.png"
+  }
+]);
